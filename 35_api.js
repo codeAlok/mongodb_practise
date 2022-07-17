7a75b553134e6a0442c6c394712c97fc8847679d
alok <alok60541@gmail.com>
2022-07-17 14:52:47 +0530

    post request api

--- 35_api.js
+++ 35_api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dbConnect = require('./mongodb');
-
 const app = express();
+
+app.use(express.json());  // (to get data from request)
+// express.json() is used instead of body-parser() after express version 4.6  to use (req.body)
 
 app.get('/', async (req, res)=>{
 
@@ -12,5 +14,13 @@ app.get('/', async (req, res)=>{
     res.send(data);
 });
 
+// ***** post method *****
+app.post('/', async (req, res)=>{
+    
+    let data = await dbConnect();
+    let result = await data.insertMany(req.body); //data sent from postman/reactjs to node js
+
+    res.send(result); 
+})
 
 app.listen(5000);
